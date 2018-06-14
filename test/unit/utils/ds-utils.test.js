6b8386c66fcb889910cbfbaa5535cb50cf6cbb1a
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2018-06-14 14:19:57 +0530

    Update ds-utils.test.js

--- test/unit/utils/ds-utils.test.js
+++ test/unit/utils/ds-utils.test.js
@@ -8,8 +8,7 @@ describe('ds-utils', function() {
     let flat = [{id:1}, {id:2, parent: 1}];
     let output = dsUtils.flatToHierarchy(flat);
     assert.equal(output[0].id, 1);
-    assert.equal(output[0].chidren[0].id, 2);
+    assert.equal(output[0].children[0].id, 2);
   });
-
 
 });
