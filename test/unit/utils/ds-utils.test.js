059634481d5811ec0bcbe45faa501ac739c123c0
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2018-06-14 14:15:43 +0530

    Create ds-utils.test.js

---
+++ test/unit/utils/ds-utils.test.js
@@ -0,0 +1,15 @@
+const {assert} = require('chai');
+const fs = require('fs');
+const dsUtils = require('../../../src/utils/ds-utils.js');
+
+describe('ds-utils', function() {
+
+  it('#flatToHierarchy()', function() {
+    let flat = [{id:1}, {id:2, parent: 1}];
+    let output = dsUtils.flatToHierarchy(flat);
+    assert.equal(output[0].id, 1);
+    assert.equal(output[0].chidren[0].id, 2);
+  });
+
+
+});
