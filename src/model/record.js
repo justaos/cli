af7251ff63dcc5fd64887100a9a505ea6ba8a5e5
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2018-07-16 15:50:15 +0530

    Create record.js

---
+++ src/model/record.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Q = require('q');
+const logger = require('../config/logger');
+
+class Record {
+
+    constructor(record, fields) {
+        fields.forEach(field => {
+          this[field.name] = record[field.name];
+          if(field.display_value)
+            this._private.displayField = field;
+        });
+    }
+    
+    getDisplayValue() {
+      return this[this._private.displayField.name];
+    }
+
+}
+
+module.exports = Query;
