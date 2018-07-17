21eec45ee29c531b4c9c3073c3870193183eed12
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2018-07-17 17:41:50 +0530

    Update platform-constants.js

--- src/platform/platform-constants.js
+++ src/platform/platform-constants.js
@@ -1,12 +1,16 @@
 const config = require('../config/config');
 
-let constants = {};
+const constants = {};
 
-constants.DEFAULT_FIELDS_PATH = config.root + '/resources/platform/default-fields.json';
-if (config.mode === 'internal')
-    constants.PROD_PATH = config.cwd + '/resources/apps/';
-else
-    constants.PROD_PATH = config.cwd + '/apps/';
+constants.path = {};
+constants.path.APPS = config.cwd + '/resources/apps/';
 
+constants.path.PATFORM_RESOUCES = config.root + '/resources/platform';
+constants.path.DEFAULT_FIELDS = constants.path.PATFORM_RESOUCES + '/default-fields.json';
+constants.path.PATFORM_MODELS = constants.paths.PATFORM_RESOUCES + '/models';
+
+constants.path.P_COLLECTION_MODEL = constants.paths.PATFORM_MODELS + '/p_collection.json';
+constants.path.P_FIELD_MODEL = constants.paths.PATFORM_MODELS + '/p_field.json';
+constants.path.P_OPTION_MODEL = constants.paths.PATFORM_MODELS + '/p_option.json';
 
 module.exports = constants;
