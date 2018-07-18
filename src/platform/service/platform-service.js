dc959b34c94b7e6257475de5b0737a43081c1fdd
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2018-07-18 15:16:47 +0530

    Update platform-service.js

--- src/platform/service/platform-service.js
+++ src/platform/service/platform-service.js
@@ -31,10 +31,10 @@ class PlatformService extends BaseService {
                    promises.push(dfd.promise);
                    let aclRolesForMenu = acls.filter(acl => acl.record_id == menu.id).map(acl => acl.id);
                    if(aclRolesForMenu.length){
-                       aclRoleModel.find({acl: aclRolesForMenu[0].id}).then(function(aclRoles){
+                       aclRoleModel.find({acl: aclRolesForMenu[0].id}).exec(function(err, aclRoles){
                            let flag = false;
                            aclRoles.forEach(function(aclRole){
-                               if(this.sessionUser.hasRoleId(aclRole)){
+                               if(that.sessionUser.hasRoleId(aclRole)){
                                    flag = true;
                                }
                            });
