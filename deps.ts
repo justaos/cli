0049290808a4aecd97d7ed139a35cce4f5fdac04
Shivaji Varma Venkata Pusapati <contact@shivajivarma.com>
2023-01-31 12:21:40 +0530

    index issue fix

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 import FileUtils from "https://deno.land/x/justaos_utils@1.3.0/file-utils/mod.ts";
 import * as path from "https://deno.land/std@0.170.0/path/mod.ts";
-import { expandGlobSync } from "https://deno.land/std@0.170.0/fs/mod.ts";
+import { walkSync } from "https://deno.land/std@0.170.0/fs/mod.ts";
 import { Command } from "https://deno.land/x/cliffy@v0.24.2/command/command.ts";
 import { wait } from "https://deno.land/x/wait@0.1.12/mod.ts";
 import { fromStreamReader } from "https://deno.land/std@v0.60.0/io/streams.ts";
 
-export { Command, expandGlobSync, FileUtils, fromStreamReader, path, wait };
+export { Command, walkSync, FileUtils, fromStreamReader, path, wait };
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,17 +1,22 @@
-import { Command, expandGlobSync, FileUtils, path } from "../../deps.ts";
+import { Command, FileUtils, path, walkSync } from "../../deps.ts";
+
 
 const indexCommand = new Command()
   .description("Creates index of json files in a directory")
   .action((options: any) => {
     const files = [];
-    for (const file of expandGlobSync("**/*.json")) {
-      if (file.path.indexOf("node_modules") == -1) {
-        files.push(path.normalize(file.path).replace(Deno.cwd(), ""));
-      }
+    for (const file of walkSync("./",
+      {
+        maxDepth: 5,
+        includeDirs: false,
+        exts: [".json"],
+        skip: [/node_modules/]
+      })) {
+      files.push("\\" + path.normalize(file.path).replace(Deno.cwd(), ""));
     }
     FileUtils.writeTextFileSync(
       "./folder-index.json",
-      JSON.stringify(files, null, 2),
+      JSON.stringify(files, null, 2)
     );
     console.log(`Indexed ${files.length} files in ./folder-index.json`);
   });
