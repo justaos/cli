82b478a9f178b3981fd62b24a44d6719af1c9102
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2023-09-01 22:57:45 +0530

    file index api update

--- scratch.ts
+++ scratch.ts
@@ -1,4 +1,19 @@
-const p = Deno.run({
+/*const p = Deno.run({
   cmd: ["cmd", "/c", "justaos", "--version"],
 });
-const status = await p.status();
+const status = await p.status();*/
+
+
+import { walkSync } from "https://deno.land/std@0.198.0/fs/walk.ts";
+import * as path from "https://deno.land/std@0.198.0/path/mod.ts";
+
+const currentPath = new URL('.', import.meta.url);
+for (const file of walkSync(currentPath,
+  {
+    maxDepth: 5,
+    includeDirs: false,
+    exts: [".json"],
+    skip: [/node_modules/]
+  })) {
+  console.log(path.toFileUrl(file.path).pathname.replace(currentPath.pathname, ''));
+}
